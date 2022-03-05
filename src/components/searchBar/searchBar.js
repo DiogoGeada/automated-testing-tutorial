3098167e21a5e8ff306370fb9f4ee6fd276c1f06
Diogo Geada <diogocgeada@gmail.com>
2022-03-05 20:37:47 +0100

    Search bar is functional

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,12 +1,37 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import actions from '../../redux/actions';
 import './searchBar.css';
 
-export default function SearchBar(){
-    return(
-        <div className="search" >
-            <input type="text" placeholder="Search for a cocktail..."/>
-            <button>
-                <i className="fa fa-search"></i>
-            </button>
-      </div>
-    );
+class SearchBar extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            keywords: ""
+        }
+    }
+
+    render(){
+        return(
+            <div className="search" >
+                <input type="text" placeholder="Search for a cocktail..." value={this.state.keywords} 
+                onChange={e => this.setState({"keywords": e.target.value})}
+                onKeyPress={e => {
+                    if(e.key == "Enter") 
+                        this.props.search(this.state.keywords)
+                    }}
+                />
+                <button onClick={() => this.search(this.state.keywords)}>
+                    <i className="fa fa-search"></i>
+                </button>
+        </div>
+        );
+    }
 }
+
+const mapDispatchToProps = dispatch => ({
+    search: drink => dispatch(actions.getDrinks(drink)) 
+})
+
+export default connect(null, mapDispatchToProps)(SearchBar);
