ccdbdba2f48585c3e91c243023302dad77424d9c
Diogo Geada <diogocgeada@gmail.com>
2022-03-09 01:11:55 +0100

    Prepared tests for presentation

--- src/components/grid/grid.integration.test.js
+++ src/components/grid/grid.integration.test.js
@@ -1,10 +1,12 @@
 import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Home from "../../views/home/home";
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import { COCKTAIL_LIBRARY_URL } from "../../services/cocktailService";
 import cocktails from "../data/drinks.json";
+
+// msw
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 // Redux
 import { Provider } from "react-redux";
@@ -15,58 +17,4 @@ import drinksReducer from "../../redux/reducers";
 
 describe("Cocktail Grid", () => {
 
-    let server = setupServer(
-        rest.get(COCKTAIL_LIBRARY_URL, (req, res, ctx) => {
-            return res(ctx.json(cocktails))
-        })
-    );
-    let store = {};
-
-    beforeAll(() => server.listen());
-    beforeEach(() => {
-        store = newStore();
-    })
-
-    afterEach(() => {
-        server.resetHandlers()
-        store = {};
-    })
-    afterAll(() => server.close());
-
-    it("Renders items", async () => {
-    
-        render(home());
-
-        let loading = screen.getByText("Loading...");
-        await waitForElementToBeRemoved(loading);
-
-        cocktails.drinks.forEach(cocktail => {
-            expect(screen.getByText(cocktail.strDrink.substring(0, 20))).toBeInTheDocument();
-        });
-    });
-
-    it("Displays loading message", async () => {
-
-        render(home());
-        expect(screen.getByText("Loading...")).toBeInTheDocument();
-
-        await waitFor(() => {
-            expect(screen.getByText(cocktails.drinks[0].strDrink)).toBeInTheDocument();
-        });
-        
-    })
-
-    const home = () => {
-        return (
-        <Provider store={store}>
-            <BrowserRouter location={{pathname: "/"}}>
-                <Home />
-            </BrowserRouter>
-        </Provider>);
-    } 
-
-    const newStore = () => {
-        let middleware = applyMiddleware(fetchDrinkMiddleware); 
-        return createStore(drinksReducer, {...initialState}, middleware);
-    }
 });
