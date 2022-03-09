9ef662c994e7fd6a3937f3fc518e0635003ba757
Diogo Geada <diogocgeada@gmail.com>
2022-03-09 01:18:45 +0100

    Recovered cocktail server previous version

--- src/services/cocktailService.js
+++ src/services/cocktailService.js
@@ -6,6 +6,13 @@ export default class CocktailService {
         .then(data => data.drinks)
         .catch(e => e);
     }
+
+    static async getDrink(drinkId){
+        return await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
+        .then(data => data.json())
+        .then(data => data.drinks[0])
+        .catch(e => e);
+    }
 }
 
 export const COCKTAIL_LIBRARY_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php"
